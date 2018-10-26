0e117c76d1c0bf9117f55ee1318bf499e5468216
Carlos J. Soto <csoto09@gmail.com>
2018-10-26 17:44:21 -0400

    create form to add rooms to RoomList array in FireBase. Assignment 2 complete.

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,7 +4,8 @@ class RoomList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            rooms: []
+            rooms: [],
+            newRoomName: ''
         }
         this.roomsRef = this.props.firebase.database().ref('rooms');
 
@@ -15,14 +16,32 @@ class RoomList extends Component {
             room.key = snapshot.key;
             this.setState({ rooms: this.state.rooms.concat( room ) });
         });
-      }
+    }
+    handleChange(e) {
+        this.setState({ newRoomName: e.target.value })
+    }
+    createRoom(e) {
+        e.preventDefault();
+        if(!this.state.newRoomName) { return }
+        const newRoom = this.state.newRoomName;
+        this.roomsRef.push({
+            name: newRoom 
+        });  
+        this.setState({newRoomName:''});
+    }
     render() {
     return (
-            <ul className = "roomList">
-                { this.state.rooms.map( (room) => {
-                    return <li key={room.key}>{room.name}</li>;
-                } )}
+            <div>
+                <ul className = "roomList">
+                    { this.state.rooms.map( (room) => {
+                        return <li key={room.key}>{room.name}</li>;
+                     } )}
             </ul>
+                <form onSubmit={ (e) => this.createRoom(e) }>
+                    <input type="text" value={ this.state.newRoomName } onChange={ (e) => this.handleChange(e) } />
+                    <input type="submit"/>
+                </form>
+            </div>
     )
     }
 }
